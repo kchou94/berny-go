7755e71f82690ed248f3b9fd2d859a942a47f633
Berny Linville <bernylinville@devopsthink.org>
2022-10-09 18:37:01 +0800

    add 继承

---
+++ oop/inheritance/inheritance.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+	sex  string
+}
+
+func (Person) SayHello() {
+	fmt.Println("This is from Person")
+}
+
+type Student struct {
+	Person
+	school string
+}
+
+func main() {
+	stu := Student{school: "清华大学"}
+	stu.name = "张三"
+	stu.age = 18
+	// stu.sex = "男"
+	fmt.Println(stu)
+
+	stu.SayHello()
+}

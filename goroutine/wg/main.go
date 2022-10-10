d674b189114d5868a1fae1a3fb93ac84bffbb12c
Berny Linville <bernylinville@devopsthink.org>
2022-10-10 10:18:33 +0800

    add 开启多个协程

--- goroutine/wg/main.go
+++ goroutine/wg/main.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup // 声明一个计数器
+var wg sync.WaitGroup // 第一步：声明一个计数器
 
 func test1() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("test1() i = ", i)
 		time.Sleep(time.Millisecond * 100)
 	}
-	wg.Done() // 计数器减一
+	wg.Done() // 第三步：计数器减一
 }
 
 func test2() {
@@ -25,10 +25,10 @@ func test2() {
 }
 
 func main() {
-	wg.Add(1) // 计数器加一
+	wg.Add(1) // 第二步：计数器加一
 	go test1()
 	wg.Add(1) // 计数器加一
 	go test2()
-	wg.Wait() // 等待协程结束
-	fmt.Println("主线程推出...")
+	wg.Wait() // 第四步：等待协程结束
+	fmt.Println("主线程退出...")
 }
